cc016faf38d91e73b5819135b2ff142ad24dfb86
Your GitHub Username <Your GitHub Email>
2025-01-06 22:31:44 +0500

    boshlanishi

---
+++ main.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.container')
+for (let i = 0; i < 50; i++) {
+  const particle = document.createElement('div')
+  particle.classList.add('particle')
+  particle.style.left = `${Math.random() * 100}%`
+  particle.style.top = `${Math.random() * 100}%`
+  particle.style.animationDelay = `${i * 8}s`
+  particle.style.width = `${Math.random() * 6 + 2}px`
+  particle.style.height = particle.style.width
+
+  container.appendChild(particle)
+}
