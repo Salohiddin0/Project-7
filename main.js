646f34581b2547703a19819b5b18a0f9f7de4b82
Your GitHub Username <Your GitHub Email>
2025-01-06 22:33:45 +0500

    Floating orb added

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ for (let i = 0; i < 50; i++) {
   particle.classList.add('particle')
   particle.style.left = `${Math.random() * 100}%`
   particle.style.top = `${Math.random() * 100}%`
-  particle.style.animationDelay = `${i * 8}s`
+  particle.style.animationDelay = `${i * 0.1}s`
   particle.style.width = `${Math.random() * 6 + 2}px`
   particle.style.height = particle.style.width
 
